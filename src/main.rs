2d99da3e1a062d2e7814250f5d0e8ff80fa59797
martin-creator <martinlubowa@outlook.com>
2023-10-27 23:46:06 +0300

    Added mutable option on rust

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ fn divide_by_two(num: u32)->u32{
 
 fn main() {
     let x:u32 = 10;
-    let a = add_five(x);
-    let b:u32 = divide_by_two(a);
+    let mut a = add_five(x);
+    let mut b:u32 = divide_by_two(a);
+    
+    a = 100;
 
     println!("The value of a is: {}", a);
     println!("The value of b is: {}", b);
