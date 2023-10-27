942981d5eeb71259600a3e43d4ab51018eea3a9a
martin-creator <martinlubowa@outlook.com>
2023-10-27 23:43:38 +0300

    Added divide function

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ fn add_five(num: u32)->u32{
     num + 5
 }
 
+fn divide_by_two(num: u32)->u32{
+    num / 2
+}
+
 fn main() {
     let x:u32 = 10;
     let a = add_five(x);
+    let b:u32 = divide_by_two(a);
 
     println!("The value of a is: {}", a);
+    println!("The value of b is: {}", b);
 }
