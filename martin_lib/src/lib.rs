0fc6dd076b6544395536e5e696d394bdf0ae4376
martin-creator <martinlubowa@outlook.com>
2023-10-28 00:51:29 +0300

    Created library

--- martin_lib/src/lib.rs
+++ martin_lib/src/lib.rs
@@ -1,3 +1,22 @@
+/// Adds left and right and returns the result.
+/// 
+/// # Arguments
+/// 
+/// * `left` - The left hand side of the addition.
+/// * `right` - The right hand side of the addition.
+/// 
+/// # Examples
+/// 
+/// ```
+/// use martin_lib::add;
+/// 
+/// let result = add(2, 2);
+/// assert_eq!(result, 4);
+/// ```
+/// 
+
+
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
