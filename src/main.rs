b3b3d27c903739952d2a853ae681a44f96e041b7
martin-creator <martinlubowa@outlook.com>
2023-10-27 23:42:16 +0300

    Addded  add functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn add_five(num: u32)->u32{
+    num + 5
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x:u32 = 10;
+    let a = add_five(x);
+
+    println!("The value of a is: {}", a);
 }
