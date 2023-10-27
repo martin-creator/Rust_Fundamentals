e1c7c53b11d0f058ce04aefc00fe067b27ae4d87
martin-creator <martinlubowa@outlook.com>
2023-10-28 00:15:44 +0300

    Added test for divide function

--- src/other_funcs/divide_funcs.rs
+++ src/other_funcs/divide_funcs.rs
@@ -1,3 +1,19 @@
 pub fn divide_by_two(num: u32)->u32{
     num / 2
 }
+
+
+#[cfg(test)]
+
+mod test {
+    use super::*;
+
+    #[test]
+
+    fn divide_by_two_test(){
+        let x:u32 = 100;
+        let y = divide_by_two(x);
+        print!("y is from divide_by_two_test: {} {}", y, x);
+        assert_eq!(y, 50);
+    }
+}
