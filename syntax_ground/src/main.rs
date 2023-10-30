94e9a16337ab1bf1ba4833d818521da59844115f
martin-creator <martinlubowa@outlook.com>
2023-10-31 00:57:24 +0300

    Added arrays

--- syntax_ground/src/main.rs
+++ syntax_ground/src/main.rs
@@ -1,173 +1,198 @@
 const OUR_COURSE: &str = "Rust Programming Language"; // This string is a static string stored on the stack
 
 fn main() {
-    println!("Welcome to this course on {}", OUR_COURSE);
+//     println!("Welcome to this course on {}", OUR_COURSE);
 
-    // stack
-    let x: i32;
-    x = 42;
-    println!("x = {}", x);
+//     // stack
+//     let x: i32;
+//     x = 42;
+//     println!("x = {}", x);
 
-    // heap
-    let mut s = String::from("Hello");
-    s.push_str(", world!");
-    println!("{}", s);
+//     // heap
+//     let mut s = String::from("Hello");
+//     s.push_str(", world!");
+//     println!("{}", s);
 
 
-    // stack
-    let y: i32;
-    y = 4;
-    println!("y = {}", y);
+//     // stack
+//     let y: i32;
+//     y = 4;
+//     println!("y = {}", y);
 
-    // For loop
-    for i in 0..=y {
-        if i % 2 == 0 {
-            println!("{} is even", i);
-        } else {
-            println!("{} is odd", i);
-        }
-    }
+//     // For loop
+//     for i in 0..=y {
+//         if i % 2 == 0 {
+//             println!("{} is even", i);
+//         } else {
+//             println!("{} is odd", i);
+//         }
+//     }
 
-    // While loop
-    let mut z = 0;
-    while z < 5 {
-        println!("z = {}", z);
-        z += 1;
-    }
+//     // While loop
+//     let mut z = 0;
+//     while z < 5 {
+//         println!("z = {}", z);
+//         z += 1;
+//     }
 
-    // If statement
-    if z == 5 {
-        println!("z is 5");
-    } else {
-        println!("z is not 5");
-    }
+//     // If statement
+//     if z == 5 {
+//         println!("z is 5");
+//     } else {
+//         println!("z is not 5");
+//     }
 
-    // if else if else
-    if z == 5 {
-        println!("z is 5");
-    } else if z == 6 {
-        println!("z is 6");
-    } else {
-        println!("z is not 5 or 6");
-    }
+//     // if else if else
+//     if z == 5 {
+//         println!("z is 5");
+//     } else if z == 6 {
+//         println!("z is 6");
+//     } else {
+//         println!("z is not 5 or 6");
+//     }
 
-    // match statement
-    match z {
-        1 => println!("z is 1"),
-        2 => println!("z is 2"),
-        3 => println!("z is 3"),
-        4 => println!("z is 4"),
-        5 => println!("z is 5"),
-        6 => println!("z is 6"),
-        _ => println!("z is not 1-6"),
-    }
+//     // match statement
+//     match z {
+//         1 => println!("z is 1"),
+//         2 => println!("z is 2"),
+//         3 => println!("z is 3"),
+//         4 => println!("z is 4"),
+//         5 => println!("z is 5"),
+//         6 => println!("z is 6"),
+//         _ => println!("z is not 1-6"),
+//     }
 
-    // function call
-    fn add(x: i32, y: i32) -> i32 {
-        x + y
-    }
-    let sum = add(2, 3);
-    println!("sum = {}", sum);
+//     // function call
+//     fn add(x: i32, y: i32) -> i32 {
+//         x + y
+//     }
+//     let sum = add(2, 3);
+//     println!("sum = {}", sum);
 
-    // ownership
-    let s1 = String::from("Hello");
-    let s2 = s1;
-    // println!("{}", s1); // This will not work because s1 has been moved to s2
-    println!("{}", s2);
+//     // ownership
+//     let s1 = String::from("Hello");
+//     let s2 = s1;
+//     // println!("{}", s1); // This will not work because s1 has been moved to s2
+//     println!("{}", s2);
 
-    // borrowing
-    let s3 = String::from("Hello");
-    let s4 = &s3;
-    println!("{}", s3);
+//     // borrowing
+//     let s3 = String::from("Hello");
+//     let s4 = &s3;
+//     println!("{}", s3);
 
-    // mutable borrowing
-    let mut s5 = String::from("Hello");
-    let s6 = &mut s5;
-    s6.push_str(", world!");
-    println!("{}", s6);
+//     // mutable borrowing
+//     let mut s5 = String::from("Hello");
+//     let s6 = &mut s5;
+//     s6.push_str(", world!");
+//     println!("{}", s6);
 
-    // structs
-    struct Person {
-        name: String,
-        age: u8,
-    }
+//     // structs
+//     struct Person {
+//         name: String,
+//         age: u8,
+//     }
 
-    let p1 = Person {
-        name: String::from("John"),
-        age: 42,
-    };
+//     let p1 = Person {
+//         name: String::from("John"),
+//         age: 42,
+//     };
 
-    println!("p1's name is {}", p1.name);
+//     println!("p1's name is {}", p1.name);
 
-    // enums
-    enum Direction {
-        Up,
-        Down,
-        Left,
-        Right,
-    }
+//     // enums
+//     enum Direction {
+//         Up,
+//         Down,
+//         Left,
+//         Right,
+//     }
 
-    let player_direction: Direction = Direction::Down;
+//     let player_direction: Direction = Direction::Down;
 
-    match player_direction {
-        Direction::Up => println!("We are heading up!"),
-        Direction::Down => println!("We are heading down!"),
-        Direction::Left => println!("We are heading left!"),
-        Direction::Right => println!("We are heading right!"),
-    }
+//     match player_direction {
+//         Direction::Up => println!("We are heading up!"),
+//         Direction::Down => println!("We are heading down!"),
+//         Direction::Left => println!("We are heading left!"),
+//         Direction::Right => println!("We are heading right!"),
+//     }
 
-    // vectors
-    let mut v: Vec<i32> = Vec::new();
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    println!("v = {:?}", v);
+//     // vectors
+//     let mut v: Vec<i32> = Vec::new();
+//     v.push(5);
+//     v.push(6);
+//     v.push(7);
+//     println!("v = {:?}", v);
 
-    // closures
-    let square = |x: i32| x * x;
-    println!("square(2) = {}", square(2));
+//     // closures
+//     let square = |x: i32| x * x;
+//     println!("square(2) = {}", square(2));
 
-    // traits
-    trait Animal {
-        fn name(&self) -> &'static str;
-        fn talk(&self) {
-            println!("{} cannot talk", self.name());
-        }
-    }
+//     // traits
+//     trait Animal {
+//         fn name(&self) -> &'static str;
+//         fn talk(&self) {
+//             println!("{} cannot talk", self.name());
+//         }
+//     }
 
-    struct Human {
-        name: &'static str,
-    }
+//     struct Human {
+//         name: &'static str,
+//     }
 
-    impl Animal for Human {
-        fn name(&self) -> &'static str {
-            self.name
-        }
+//     impl Animal for Human {
+//         fn name(&self) -> &'static str {
+//             self.name
+//         }
 
-        fn talk(&self) {
-            println!("{} says hello", self.name());
-        }
-    }
+//         fn talk(&self) {
+//             println!("{} says hello", self.name());
+//         }
+//     }
 
-    let h = Human { name: "John" };
-    h.talk();
+//     let h = Human { name: "John" };
+//     h.talk();
 
-let freezing_temp = -2.4;
-let boiling_temp = 100.0;
+// let freezing_temp = -2.4;
+// let boiling_temp = 100.0;
 
-if freezing_temp < 0.0 {
-    println!("Water is frozen")
-} else if boiling_temp > 100.0 {
-    println!("Water is boiling");
-} else {
-    println!("Water is liquid");
-}
+// if freezing_temp < 0.0 {
+//     println!("Water is frozen")
+// } else if boiling_temp > 100.0 {
+//     println!("Water is boiling");
+// } else {
+//     println!("Water is liquid");
+// }
 
-let my_inits: [char; 3] = ['J', 'C', 'D']; // This is a static array stored on the stack
-println!("My initials are {}{}{}", my_inits[0], my_inits[1], my_inits[2]);
+// let my_inits: [char; 3] = ['J', 'C', 'D']; // This is a static array stored on the stack
+// println!("My initials are {}{}{}", my_inits[0], my_inits[1], my_inits[2]);
 
-let my_floats = [1.0, 2.0, 3.0]; // This is a static array stored on the stack
-let my_new_floats = my_floats.map(|x: f64| x * 2.0); // This is a static array stored on the stack
-println!("my_new_floats = {:?}", my_new_floats);
+// let my_floats = [1.0, 2.0, 3.0]; // This is a static array stored on the stack
+// let my_new_floats = my_floats.map(|x: f64| x * 2.0); // This is a static array stored on the stack
+// println!("my_new_floats = {:?}", my_new_floats);
+
+let name:&str = "Martin Lubowa";
+let age:u8 = 23;
+let is_tall:bool = true;
+println!("My name is {} and I am {} years old. Am I tall? {}", name, age, is_tall);
+
+let dynamic_name = String::from("Martin Lubowa");
+println!("My dynamic_name is {}", dynamic_name);
+print!("My dynamic_name is stored at {:p}", &dynamic_name);
+
+let str_slice:&str = &dynamic_name[0..6];
+println!("My str_slice is {}", str_slice);
+
+
+let mut chars: Vec<char> = Vec::new();
+chars.insert(0, 'M');
+chars.insert(1, 'a');
+chars.insert(2, 'r');
+chars.insert(3, 't');
+chars.insert(4, 'i');
+chars.insert(5, 'n');
+chars.push(' ');
+
+println!("{:?}", chars);
+dbg!(chars);
 
 }
