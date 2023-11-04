09a5dc7bb088008454dce099dd022ca55d96fad5
martin-creator <martinlubowa@outlook.com>
2023-11-05 00:29:43 +0300

    Created car color enum

--- syntax_ground/src/m1_enums.rs
+++ syntax_ground/src/m1_enums.rs
@@ -1,5 +1,15 @@
+#[derive(Debug)]
+enum CarColour{
+    Red,
+    Green,
+    Blue,
+    Silver,
+}
 
-
+fn create_car_colour_Red() -> CarColour {
+    let my_car_colour = CarColour::Red; // this is a copy
+    my_car_colour
+}
 
 
 
@@ -11,7 +21,8 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_1() {
-        assert_eq!(1, 1);
+    fn test_enums() {
+        let my_car_colour = create_car_colour_Red();
+        println!("my_car_colour = {:?}", my_car_colour);
     }
 }
