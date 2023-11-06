ea81460614648467edc694f76a3c76174125ca38
martin-creator <martinlubowa@outlook.com>
2023-11-06 16:25:54 +0300

    Finished pattern matching

--- syntax_ground/src/m6_patterns.rs
+++ syntax_ground/src/m6_patterns.rs
@@ -1,81 +1,128 @@
-
-
-
-
-
-
-
-
 #[cfg(test)]
-
 mod test {
-    
-    use super::*;
 
-    #[test]
-    fn test_patterns () {
-
-        let number : i32 = 20;
-
-        // match number {
-        //     1 => print!("Ths is the first number!"),
-        //     2|3|5|7|11|15|20 => print!("We found it in the sequence!"),
-        //     13..=19 => print!("We found it in the range!"),
-        //     _ => print!("We didn't find it!"),
-        // }
-       
-       let res:&str = match number {
-           1 => "Ths is the first number!",
-           2|3|5|7|11|15|20 => "We found it in the sequence!",
-           13..=19 => "We found it in the range!",
-           _ => "We didn't find it!",      
-         };
-
-         print!("{}", res);
+  #[derive(Debug)]
+  enum Message {
+    Quit,
+    ChangeColour(i32, i32, i32),
+    Move { x: i32, y: i32 },
+    Write(String)
+  }
+  
+  fn process_message(msg: Message) {
+    match msg {
+      Message::Quit => {
+        println!("I quit!");
+      },
+      Message::ChangeColour(red, green, blue) => {
+        println!("Red {}, Green {}, Blue {}", red, green, blue);
+      },
+      Message::Move { x, y: new_name } => {
+        println!("X is {}, Y as new_name is {}", x, new_name);
       }
+      Message::Write(text) => {
+        println!("{}", text);
+      }
+    };
+  }
 
-        #[test]
+  #[test]
+  fn tests_large_enum() {
+    let _my_quit: Message = Message::Quit;
+    let _my_colour: Message = Message::ChangeColour(10, 20, 255);
+    let _my_move: Message = Message::Move { x: 10, y: 200 };
+    let my_write: Message = Message::Write("My awesome string".to_string());
+    process_message(my_write);
+  }
 
-        fn test_match_options(){
+  #[test]
+  fn tests_match_literals() {
+    
+    let number: i32 = 20;
 
-            let some_number: Option<i32> = None;
-            //let prob_none: Option<i32> = None;
+    let res: &str = match number {
+      1 => "It was the first!",
+      2 | 3 | 5 | 7 | 15 | 20 => "We found it in the sequence!",
+      _ => "It was something else!"
+    };
 
-           let my_int:i32 = if let Some(i) = some_number {
-            i
-            } else {
-                panic!("We found a None!")
-            };
+    println!("{}", res);
+  }
 
-            print!("{}", my_int);
+  #[test]
+  fn tests_match_option() {
 
-            // let res:i32 = match some_number{
-            //     Some(i) => i,
-            //     None => {
-            //         panic!("We found a None!")
-            //     }
-            // };
+    let some_num: Option<i32> = Some(10);
 
-            // print!("{}", res);
-            // print!("{:?}", some_number);
-            
+    let my_int: i32 = if let Some(i) = some_num {
+      i
+    } else {
+      panic!("There was a problem");
+    };
 
-        }
+    println!("My int: {}", my_int);
 
-        #[test]
+    // let res = match some_num {
+    //   Some(i) => i,
+    //   None => {
+    //     panic!("There was a problem");
+    //   }
+    // };
 
-        fn tests_match_result() {
+    // println!("{:?}", some_num);
+    // println!("{}", res);
+  }
 
-            let some_result: Result<i32, String> = Ok(42);
-            let some_err: Result<i32, &str> = Err("We found an error!");
 
-            let res = match some_res {
-                Ok(i) => i,
-                Err(e) => panic!("{}", e),
-            };
+  #[test]
+  fn tests_match_result() {
 
-            print!("{}", res);
-        
-            }
-        }
+    let some_res: Result<i32, &str> = Ok(50);
+    let _some_err: Result<i32, &str> = Err("There was a problem");
 
+    let res = match some_res {
+      Ok(val) => val,
+      Err(e) => panic!("{}", e)
+    };
+
+    println!("{}", res);
+
+    let my_int: i32 = if let Ok(r) = some_res {
+      r
+    } else {
+      panic!("There was a problem");
+    };
+
+    println!("{}", my_int);
+  }
+
+  #[test]
+  fn tests_match_guard() {
+    let pair: (i32, i32) = (2, -2);
+    match pair {
+      (x, y) if x == y => println!("They match!"),
+      (x, y) if x + y == 0 => println!("They neutralize"),
+      (_, y) if y == 2 => println!("Y is indeed +2"),
+      _ => println!("We are no bothered")
+    };
+  }
+
+  #[test]
+  #[allow(unused_variables)]
+  fn tests_match_struct() {
+
+    struct Location {
+      x: i32,
+      y: i32
+    }
+
+    let location: Location = Location { x: 0, y: 20 };
+
+    match location {
+      Location { x, y: 0 } => println!("Y is on the axis"),
+      Location { x: 0, y } => println!("X is on the axis"),
+      Location { x, y } => println!("X and Y are not on the axis"),
+    };
+
+  }
+}
