143ef92a697089d21a1affb72083e7fe25c29396
martin-creator <martinlubowa@outlook.com>
2023-11-10 21:02:07 +0300

    Added trouble shooting for Open call test function

--- auto_gipty/src/apis/call_request.rs
+++ auto_gipty/src/apis/call_request.rs
@@ -1,6 +1,6 @@
-use crate::models::general::llm::{Message};
+use crate::models::general::llm::{ Message, ChatCompletion };
 use dotenv::dotenv;
-use reqwest::{Client, header::{HeaderMap, self, HeaderValue}};
+use reqwest::{ Client, header::{ HeaderMap, self, HeaderValue } };
 use std::env;
 
 // Call Large language model(.ie GTP-4)
@@ -8,30 +8,44 @@ pub async fn call_gpt(messages: Vec<Message>) {
     dotenv().ok();
 
     // Extract API Key Information
-    let api_key: String = env::var("OPEN_AI_KEY").expect("OPEN_AI_KEY not found in your environment");
-    let api_org: String = env::var("OPEN_AI_ORG").expect("OPEN_AI_ORG not found in your environment");
+    let api_key: String = env
+        ::var("OPEN_AI_KEY")
+        .expect("OPEN_AI_KEY not found in your environment");
+    let api_org: String = env
+        ::var("OPEN_AI_ORG")
+        .expect("OPEN_AI_ORG not found in your environment");
 
     // confirm endpoint
-    let url :&str = "https://api.openai.com/v1/chat/completions";
+    let url: &str = "https://api.openai.com/v1/chat/completions";
 
     // Create headers
     let mut headers: HeaderMap = HeaderMap::new();
 
     // Create api key header
-    headers.insert(
-        "authorization",
-        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap()
-    );
+    headers.insert("authorization", HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap());
 
     // Create Open AI Org header
-    headers.insert(
-        "OpenAI-Organization",
-        HeaderValue::from_str(api_org.as_str()).unwrap()
-    );
+    headers.insert("OpenAI-Organization", HeaderValue::from_str(api_org.as_str()).unwrap());
 
     // Create client
-    let client = Client::builder()
-    .default_headers(headers)
-    .build()
-    .unwrap();
+    let client = Client::builder().default_headers(headers).build().unwrap();
+
+    // Create chat completion
+    let chat_completion: ChatCompletion = ChatCompletion {
+        model: "gpt-3.5-turbo-0613".to_string(),
+        messages,
+        temperature:0.1,
+    };
+
+     // // Troubleshooting
+    let res_raw = client
+      .post(url)
+      .json(&chat_completion)
+      .send()
+      .await
+      .unwrap();
+    dbg!(res_raw.text().await.unwrap());
+    
 }
+
+
--- auto_gipty/src/models/general/llm.rs
+++ auto_gipty/src/models/general/llm.rs
@@ -1,8 +1,17 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debugm serialize, Clone)]
+#[derive(Debug, Serialize, Clone)]
 
 pub struct Message{
     pub role: String, 
     pub content: String,
+}
+
+
+#[derive(Debug, Serialize, Clone)]
+
+pub struct ChatCompletion{
+    pub model: String,
+    pub messages: Vec<Message>,
+    pub temperature: f32,
 }
