0225a0bebdacdf8e45c8e818f36fbbd4f338d2dc
martin-creator <martinlubowa@outlook.com>
2023-11-11 00:15:38 +0300

    Added logic to test urls generated by LLMS

--- auto_gipty/src/models/general/general.rs
+++ auto_gipty/src/models/general/general.rs
@@ -1,4 +1,6 @@
-use crate::models::general::llm::Message;
+use reqwest::Client;
+use serde::de::DeserializeOwned;
+use crate::{models::general::llm::Message, helpers::command_line::PrintCommand, apis::call_request::call_gpt};
 
 
 // Extend AI function to return certain specific messages
@@ -19,6 +21,53 @@ pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -
 }
 
 
+
+// Performs call to LLM GPT
+pub async fn ai_task_request(
+    msg_context: String,
+    agent_position: &str,
+    agent_operation: &str,
+    function_pass: for<'a> fn(&'a str) -> &'static str,
+) -> String {
+    // Extend AI function
+    let extended_msg: Message = extend_ai_function(function_pass, &msg_context);
+
+    // Print current status
+    PrintCommand::AICall.print_agent_message(agent_position, agent_operation);
+
+    // Get LLM response
+    let llm_response_res: Result<String, Box<dyn std::error::Error + Send>> =
+        call_gpt(vec![extended_msg.clone()]).await;
+
+    // Return Success or try again
+    match llm_response_res {
+        Ok(llm_resp) => llm_resp,
+        Err(_) => call_gpt(vec![extended_msg.clone()])
+            .await
+            .expect("Failed twice to call OpenAI"),
+    }
+}
+
+// Performs call to LLM GPT - Decoded
+pub async fn ai_task_request_decoded<T: DeserializeOwned>(
+    msg_context: String,
+    agent_position: &str,
+    agent_operation: &str,
+    function_pass: for<'a> fn(&'a str) -> &'static str,
+) -> T {
+    let llm_response: String =
+        ai_task_request(msg_context, agent_position, agent_operation, function_pass).await;
+    let decoded_response: T = serde_json::from_str(llm_response.as_str())
+        .expect("Failed to decode ai response from serde_json");
+    return decoded_response;
+}
+
+// Check whether request url is valid
+pub async fn check_status_code(client: &Client, url: &str) -> Result<u16, reqwest::Error> {
+    let response: reqwest::Response = client.get(url).send().await?;
+    Ok(response.status().as_u16())
+}
+
 #[cfg(test)]
 
 mod tests {
@@ -32,5 +81,24 @@ mod tests {
        let extended_msg =  extend_ai_function(convert_user_input_to_goal, "dummy variable");
        assert_eq!(extended_msg.role, "system".to_string());
         
+    }
+
+
+    #[tokio::test]
+    async fn tests_ai_task_request() {
+        let ai_func_param: String =
+            "Build me a webserver for making stock price api requests.".to_string();
+
+        let res: String = ai_task_request(
+            ai_func_param,
+            "Managing Agent",
+            "Defining user requirements",
+            convert_user_input_to_goal,
+        )
+        .await;
+
+        dbg!(res);
+
+        //assert!(res.len() > 20);
     }
 }
