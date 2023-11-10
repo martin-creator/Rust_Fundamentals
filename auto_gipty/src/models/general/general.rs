3ef6d96f603237cc5179a6e7a88961888bb11b76
martin-creator <martinlubowa@outlook.com>
2023-11-10 23:08:04 +0300

    Succesfully extended ai function

--- auto_gipty/src/models/general/general.rs
+++ auto_gipty/src/models/general/general.rs
@@ -1,7 +1,21 @@
+use crate::models::general::llm::Message;
 
 
-pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) { //-> Message {
+// Extend AI function to return certain specific messages
+pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
     let ai_function_str = ai_func(func_input);
+    
+    // Extend the string to encourage only printing the output
+    let msg: String = format!("FUNCTION: {}
+    INSTRUCTION: You are a function printer. You only print the results of functions
+    . Nothing else. No commentary. Here is the input to the function:{}.
+    Print out what the function will return.", ai_function_str, func_input);
+
+    // Return message
+    Message {
+        role: "system".to_string(),
+        content: msg,
+    }
 }
 
 
@@ -15,6 +29,8 @@ mod tests {
     #[test]
     fn tests_extending_ai_function(){
 
-        let x_str : &str = convert_user_input_to_goal("dummy variable")
+       let extended_msg =  extend_ai_function(convert_user_input_to_goal, "dummy variable");
+       assert_eq!(extended_msg.role, "system".to_string());
+        
     }
 }
