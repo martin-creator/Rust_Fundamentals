539c65b3e92a4dee1ded6715a4d9b070fdec0166
martin-creator <martinlubowa@outlook.com>
2023-11-11 12:33:44 +0300

    Added special functions

--- syntax_ground/my_proc_macro/web_template/src/main.rs
+++ syntax_ground/my_proc_macro/web_template/src/main.rs
@@ -2,6 +2,8 @@ use actix_cors::Cors;
 
 use actix_web::{ http::header, web, App, HttpServer, Responder, HttpResponse };
 
+use actix_files::Files;
+
 use serde::{ Deserialize, Serialize };
 
 use reqwest::Client as HttpClient;
@@ -17,27 +19,27 @@ use std::io::Write;
 struct Task {
     id: u64,
     name: String,
-    completed: bool
+    completed: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct User {
     id: u64,
     username: String,
-    password: String
+    password: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Database {
     tasks: HashMap<u64, Task>,
-    users: HashMap<u64, User>
+    users: HashMap<u64, User>,
 }
 
 impl Database {
     fn new() -> Self {
         Self {
             tasks: HashMap::new(),
-            users: HashMap::new()
+            users: HashMap::new(),
         }
     }
 
@@ -87,7 +89,7 @@ impl Database {
 }
 
 struct AppState {
-    db: Mutex<Database>
+    db: Mutex<Database>,
 }
 
 async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
@@ -101,7 +103,7 @@ async fn read_task(app_state: web::Data<AppState>, id: web::Path<u64>) -> impl R
     let db: std::sync::MutexGuard<Database> = app_state.db.lock().unwrap();
     match db.get(&id.into_inner()) {
         Some(task) => HttpResponse::Ok().json(task),
-        None => HttpResponse::NotFound().finish()
+        None => HttpResponse::NotFound().finish(),
     }
 }
 
@@ -137,46 +139,40 @@ async fn login(app_state: web::Data<AppState>, user: web::Json<User>) -> impl Re
     match db.get_user_by_name(&user.username) {
         Some(stored_user) if stored_user.password == user.password => {
             HttpResponse::Ok().body("Logged in!")
-        },
-        _ => HttpResponse::BadRequest().body("Invalid username or password")
+        }
+        _ => HttpResponse::BadRequest().body("Invalid username or password"),
     }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     let db: Database = match Database::load_from_file() {
         Ok(db) => db,
-        Err(_) => Database::new()
+        Err(_) => Database::new(),
     };
 
     let data: web::Data<AppState> = web::Data::new(AppState {
-        db: Mutex::new(db)
+        db: Mutex::new(db),
     });
 
     HttpServer::new(move || {
         App::new()
-            .wrap(
-                Cors::permissive()
-                    .allowed_origin_fn(|origin, _req_head| {
-                        origin.as_bytes().starts_with(b"http://localhost") || origin == "null"
-                    })
-                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
-                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-                    .allowed_header(header::CONTENT_TYPE)
-                    .supports_credentials()
-                    .max_age(3600)
-            )
+            .wrap(Cors::default().allow_any_origin().allow_any_method().max_age(3600))
             .app_data(data.clone())
-            .route("/task", web::post().to(create_task))
-            .route("/task", web::get().to(read_all_tasks))
-            .route("/task", web::put().to(update_task))
-            .route("/task/{id}", web::get().to(read_task))
-            .route("/task/{id}", web::delete().to(delete_task))
+            .service(
+                Files::new(
+                    "/",
+                    "/mnt/c/Users/user/rust_projects/syntax_ground/my_proc_macro/web_template/src"
+                ).index_file("index.html")
+            )
+            .route("/tasks", web::post().to(create_task))
+            .route("/tasks", web::get().to(read_all_tasks))
+            .route("/tasks", web::put().to(update_task))
+            .route("/tasks/{id}", web::get().to(read_task))
+            .route("/tasks/{id}", web::delete().to(delete_task))
             .route("/register", web::post().to(register))
             .route("/login", web::post().to(login))
     })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+        .bind("localhost:8080")?
+        .run().await
 }
