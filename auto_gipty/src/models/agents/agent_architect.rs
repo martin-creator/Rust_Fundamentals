ce8ca39cb9fe11a4ae98bb0b85567cb15c670831
martin-creator <martinlubowa@outlook.com>
2023-11-11 13:27:14 +0300

    Added logic to call external urls for architect

--- auto_gipty/src/models/agents/agent_architect.rs
+++ auto_gipty/src/models/agents/agent_architect.rs
@@ -1,26 +1,23 @@
-use crate::ai_functions::aifunc_architect::{print_project_scope, print_site_urls};
+use crate::ai_functions::aifunc_architect::{ print_project_scope, print_site_urls };
 use crate::helpers::command_line::PrintCommand;
-use crate::helpers::general::{ai_task_request_decoded, check_status_code};
-use crate::models::agent_basic::basic_agent::{AgentState, BasicAgent};
+use crate::helpers::general::{ ai_task_request_decoded, check_status_code };
+use crate::models::agent_basic::basic_agent::{ AgentState, BasicAgent };
 use crate::models::agent_basic::basic_traits::BasicTraits;
-use crate::models::agents::agent_traits::{FactSheet, ProjectScope, SpecialFunctions};
+use crate::models::agents::agent_traits::{ FactSheet, ProjectScope, SpecialFunctions };
 
 use async_trait::async_trait;
 use reqwest::Client;
 use std::time::Duration;
 
-
 // solution Archictect
 #[derive(Debug)]
-pub struct Agent {
-    attributes: BasicAgent,
-    factsheet: FactSheet,
-    agents: Vec<Box<dyn SpecialFunctions>>,
+pub struct AgentSolutionArchitect {
+    attributes: BasicAgent
+    
 }
 
-
 impl AgentSolutionArchitect {
-     pub fn new() -> Self {
+    pub fn new() -> Self {
         let attributes: BasicAgent = BasicAgent {
             objective: "Gathers information and design solutions for website development".to_string(),
             position: "Solution Architect".to_string(),
@@ -28,11 +25,41 @@ impl AgentSolutionArchitect {
             memory: vec![],
         };
 
-        Self{attributes}
-     }
+        Self { attributes }
+    }
 
-// Retrive Project Scope
-async fn call_project_scope(&mut self, factsheet:&mut FactSheet) -> ProjectScope {
-    let msg_context: String = format!("{}")
-}
+    // Retrive Project Scope
+    async fn call_project_scope(&mut self, factsheet: &mut FactSheet) -> ProjectScope {
+        let msg_context: String = format!("{}", factsheet.project_description);
+
+        let ai_response: ProjectScope = ai_task_request_decoded::<ProjectScope>(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_project_scope),
+            print_project_scope
+        ).await;
+
+        factsheet.project_scope = Some(ai_response.clone());
+        self.attributes.update_state(AgentState::Finished);
+
+        return ai_response;
+    }
+
+    // Determine External Urls
+    async fn call_determine_external_urls(
+        &mut self,
+        factsheet: &mut FactSheet,
+        msg_context: String,
+    ) {
+        let ai_response: Vec<String> = ai_task_request_decoded::<Vec<String>>(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_site_urls),
+            print_site_urls,
+        )
+        .await;
+
+        factsheet.external_urls = Some(ai_response);
+        self.attributes.state = AgentState::UnitTesting;
+    }
 }
