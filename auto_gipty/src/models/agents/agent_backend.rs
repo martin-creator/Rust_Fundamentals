dbd9adbd35fc5c66b15df69805818160c3e84022
martin-creator <martinlubowa@outlook.com>
2023-11-11 16:20:47 +0300

    Created backend code core logic

--- auto_gipty/src/models/agents/agent_backend.rs
+++ auto_gipty/src/models/agents/agent_backend.rs
@@ -7,7 +7,7 @@ use crate::helpers::general::{
     save_backend_code
 };
 
-use crate::helpers::command_line::{confirm_safe_code, PrintCommand};
+use crate::helpers::command_line::{PrintCommand};
 use crate::helpers::general::ai_task_request;
 use crate::models::agent_basic::basic_agent::{AgentState, BasicAgent};
 use crate::models::agents::agent_traits::{FactSheet, RouteObject, SpecialFunctions};
@@ -17,3 +17,47 @@ use reqwest::Client;
 use std::process::{Command, Stdio};
 use std::time::Duration;
 use tokio::time;
+
+pub struct AgentBackendDeveloper {
+    attributes: BasicAgent,
+    bug_errors: Option<String>,
+    bug_count: u8
+}
+
+impl AgentBackendDeveloper{
+    pub fn new() -> Self {
+        let attributes: BasicAgent = BasicAgent {
+            objective: "Develops backend code for webserver and json database".to_string(),
+            position: "Backend Developer".to_string(),
+            state: AgentState::Discovery,
+            memory: vec![],
+        };
+
+        Self {
+            attributes,
+            bug_errors: None,
+            bug_count: 0,
+        }
+    }
+    
+    async fn call_initial_backend_code(&mut self, factsheet: &mut FactSheet) {
+        let code_template_str: String = read_code_template_contents();
+
+        // Concatenate Instruction
+        let msg_context: String = format!(
+            "CODE TEMPLATE: {} \n PROJECT_DESCRIPTION: {} \n",
+            code_template_str, factsheet.project_description
+        );
+
+        let ai_response: String = ai_task_request(
+            msg_context,
+            &self.attributes.position,
+            get_function_string!(print_backend_webserver_code),
+            print_backend_webserver_code,
+        )
+        .await;
+
+        save_backend_code(&ai_response);
+        factsheet.backend_code = Some(ai_response);
+    }
+}
